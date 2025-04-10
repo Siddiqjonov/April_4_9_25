c7eee37661cb7aa703f94aefd846543cdc618420
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-04-10 05:44:04 +0500

    Student entity added with props

---
+++ src/ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+public class Student
+{
+	private int studentId;
+
+	public int StudentId
+	{
+		get { return studentId; }
+		set { studentId = value; }
+	}
+
+	private string fullName;
+
+	public string FullName
+	{
+		get { return fullName; }
+		set { fullName = value; }
+	}
+
+	public int Age { get; set; }
+
+    public string Password { get; set; }
+}
